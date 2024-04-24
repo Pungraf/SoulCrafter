894c653cb400a7b6ab86a62b3ed629b9b21739b1
Jan Bartłomiejczyk <44816218+Pungraf@users.noreply.github.com>
2024-04-24 21:40:49 +0200

    World time speed
    Setting world time speed with slider

---
+++ Assets/WorldManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WorldManager : MonoBehaviour
+{
+    [SerializeField] private Slider worldTimeSlider;
+
+    private float fixedDeltaTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        worldTimeSlider.onValueChanged.AddListener(delegate { WorldTimeSpeedChange(); });
+        this.fixedDeltaTime = Time.fixedDeltaTime;
+    }
+
+    // Update is called once per frame
+    public void WorldTimeSpeedChange()
+    {
+        Time.timeScale = worldTimeSlider.value;
+
+        Time.fixedDeltaTime = this.fixedDeltaTime * Time.timeScale;
+    }
+}
